b1c0a670b73f258ececc2f34a2d74f8f3a8cad23
Rocky <rocky23012001@gmail.com>
2024-06-23 17:59:34 +0530

    comment part done

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -31,7 +31,7 @@ export const signin = async (req, res, next) => {
     res
       .cookie('access_token', token, { httpOnly: true, expires: expiryDate })
       .status(200)
-      .json(rest);
+      .json({rest, token: token});
   } catch (error) {
     next(error);
   }
--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -1,10 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import question from '../assets/question.png'
 import arrow from '../assets/arrow.png'
 import { FaArrowCircleUp } from "react-icons/fa";
 import { FaRegComments } from "react-icons/fa";
 import { FaReplyAll } from "react-icons/fa";
+import axios from 'axios'
+// import { FaArrowCircleUp, FaRegComments, FaReplyAll } from 'react-icons/fa';
+import { formatDistanceToNow } from 'date-fns';
+import { useSelector } from 'react-redux';
+
+
 function Community() {
+
+    const {currentUser} = useSelector(state => state.user)
+    console.log(currentUser);
+
+
+
+    const [questions, setQuestions] = useState([]);
+    const [questionInput, setQuestionInput] = useState("");
+    const formatTimeAgo = (timestamp) => {
+        return formatDistanceToNow(new Date(timestamp), { addSuffix: true });
+    };
+
+
+    const getQuestionsAndComments = async () => {
+        try {
+            const res = await axios.get("/api/comment/allquestion");
+            setQuestions(res?.data?.data);
+            // console.log(res?.data?.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getQuestionsAndComments();
+    }, [])
+
+    const createQuestion = async () => {
+        const formData = {title: questionInput, comments : []}
+        console.log(formData);
+        try {
+            const res = await axios.post("/api/comment/question", formData, {
+                headers: {
+                    Authorization: currentUser?.token
+                }
+            })
+            console.log(res);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <>
             <div className='w-full flex justify-center items-center'>
@@ -12,7 +60,7 @@ function Community() {
                     <div className='w-1/2 text-center'>
                         ChatRoom
                     </div>
-                        <div className='w-1/2 text-center bg-[#008080] align-middle'>
+                    <div className='w-1/2 text-center bg-[#008080] align-middle'>
                         Q & A
                     </div>
                 </div>
@@ -29,11 +77,11 @@ function Community() {
                                 <p className='text-gray-600 font-normal text-4xl leading-14 tracking-tight p-10'>Ask Any Questionss</p>
                                 <div className='flex justify-center items-center pb-10'>
                                     <div className="relative mb-2  flex items-center ">
-                                        <input type="text" id="email-address-icon" class="bg-gray-50 border border-gray-300 text-[#FFA500] text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full  p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Type Something..."></input>
+                                        <input type="text" onInput={(e) => setQuestionInput(e.target.value)} id="email-address-icon" class="bg-gray-50 border border-gray-300 text-[#FFA500] text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full  p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Type Something..."></input>
                                     </div>
                                     <div>
                                         <button className=" px-3 py-1.5 bg-[#008080] text-white font-bold  text-2xl">
-                                            <img src={arrow} width="35px" />
+                                            <img src={arrow} width="35px" onClick={createQuestion}/>
                                         </button>
                                     </div>
                                 </div>
@@ -63,106 +111,55 @@ function Community() {
                         <div className='bg-[#FFEDCC] py-1 px-1'>Unanswered Q’s</div>
                         <div className='bg-[#FFEDCC] py-1 px-1'>My Answers</div>
                     </div>
-                    <div className='w-full   border-2 border-[#FFA500] md:mt-5 mt-1 md:p-5 p-1 rounded-xl'>
-                        <div className='flex m-2'>
-                            <div className='w-[45px] h-[45px] rounded-full bg-green-500 mx-3'>
-                            </div>
-                            <div>
-                                <p className='text-2xl font-bold -mb-1'>Shivam</p>
-                                <p>2 day ago</p>
-                            </div>
-                        </div>
-                        <div className=''>
-                            <p className='text-xl md:mx-6 mx-1'> Can you recommend some devotional practices for building a stronger relationship with Lord Krishna?
-                                Can you recommend some devotional practices for building a stronger relationship with Lord Krishna?
-                            </p>
-                        </div>
-                        <div className='flex mt-3'>
-                            <div className='flex ml-6 bg-[#FFEDCC]'>
-                                <FaArrowCircleUp className='flex w-6 h-6' /> <span className='text-xl ml-1'>12</span>
-                            </div>
-                            <div className='flex ml-6 bg-[#FFEDCC]'>
-                                <FaRegComments className='flex w-6 h-6' /> <span className='text-xl m1-1'>12</span>
-                            </div>
-                            <div className='flex ml-6 bg-[#FFEDCC]'>
-                                <FaReplyAll className='flex w-6 h-6' /> <span className='text-xl ml-1'>12</span>
-                            </div>
-                        </div>
-                    </div>
 
-                    <div className='w-full   border-2 border-[#FFA500] mt-5 p-5 rounded-xl'>
-                        <div className='flex m-2'>
-                            <div className='w-[45px] h-[45px] rounded-full bg-green-500 mx-3'>
+                    {questions?.map((item, key) => (
+                        <div key={key} className='w-full border-2 border-[#FFA500] mt-5 p-5 rounded-xl'>
+                            <div className='flex m-2'>
+                                <div className='w-[45px] h-[45px] rounded-full bg-green-500 mx-3'></div>
+                                <div>
+                                    <p className='text-2xl font-bold -mb-1'>{item?.userId?.name}</p>
+                                    <p>{formatTimeAgo(item?.createdAt)}</p>
+                                </div>
                             </div>
                             <div>
-                                <p className='text-2xl font-bold -mb-1'>Shivam</p>
-                                <p>2 day ago</p>
+                                <p className='text-xl mx-6'>{item?.title}</p>
                             </div>
-                        </div>
-                        <div className=''>
-                            <p className='text-xl mx-6'> Can you recommend some devotional practices for building a stronger relationship with Lord Krishna?
-                                Can you recommend some devotional practices for building a stronger relationship with Lord Krishna?
-                            </p>
-                        </div>
-                        <div className='flex mt-3'>
-                            <div className='flex ml-6 bg-[#FFEDCC]'>
-                                <FaArrowCircleUp className='flex w-6 h-6' /> <span className='text-xl ml-1'>12</span>
-                            </div>
-                            <div className='flex ml-6 bg-[#db701d]'>
-                                <FaRegComments className='flex w-6 h-6' /> <span className='text-xl m1-1'>12</span>
-                            </div>
-                            <div className='flex ml-6 bg-[#FFEDCC]'>
-                                <FaReplyAll className='flex w-6 h-6' /> <span className='text-xl ml-1'>12</span>
-                            </div>
-                        </div>
-                        <div className='bg-[#FFEDCC] h-[1px] w-7/10 mx-auto my-2 '></div>
-                        <p className='text-xl font-bold'>Answers</p>
-                        <div className='w-9/10 bg-[#FFEDCC] mt-5 p-5 rounded-xl'>
-                            <div className='flex m-2'>
-                                <div className='w-[45px] h-[45px] rounded-full bg-green-500 mx-3'>
+                            <div className='flex mt-3'>
+                                <div className='flex ml-6 bg-[#FFEDCC]'>
+                                    <FaArrowCircleUp className='flex w-6 h-6' /> <span className='text-xl ml-1'>12</span>
                                 </div>
-                                <div>
-                                    <p className='text-2xl font-bold -mb-1'>Shivam</p>
-                                    <p>2 day ago</p>
+                                <div className='flex ml-6 bg-[#db701d]'>
+                                    <FaRegComments className='flex w-6 h-6' /> <span className='text-xl ml-1'>12</span>
+                                </div>
+                                <div className='flex ml-6 bg-[#FFEDCC]'>
+                                    <FaReplyAll className='flex w-6 h-6' /> <span className='text-xl ml-1'>12</span>
                                 </div>
                             </div>
-                            <div className=''>
-                                <p className='text-xl mx-6'> Can you recommend some devotional practices for building a stronger relationship with Lord Krishna?
-                                    Can you recommend some devotional practices for building a stronger relationship with Lord Krishna?
-                                </p>
-                            </div>
+                            <div className='bg-[#FFEDCC] h-[1px] w-7/10 mx-auto my-2 '></div>
+                            <p className='text-xl font-bold'>Answers</p>
+                            {item?.comments?.length === 0 ? (
+                                <></>
+                            ) : (
+                                item?.comments?.map((comment, commentKey) => (
+                                    <div key={commentKey} className='w-9/10 bg-[#FFEDCC] mt-5 p-5 rounded-xl'>
+                                        <div className='flex m-2'>
+                                            <div className='w-[45px] h-[45px] rounded-full bg-green-500 mx-3'></div>
+                                            <div>
+                                                <p className='text-2xl font-bold -mb-1'>{comment?.userId?.name}</p>
+                                                <p>{formatTimeAgo(comment?.createdAt)}</p>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <p className='text-xl mx-6'>{comment?.text}</p>
+                                        </div>
+                                    </div>
+                                ))
+                            )}
                         </div>
-                        <div className='w-9/10 bg-[#FFEDCC] mt-5 p-5 rounded-xl'>
-                            <div className='flex m-2'>
-                                <div className='w-[45px] h-[45px] rounded-full bg-green-500 mx-3'>
-                                </div>
-                                <div>
-                                    <p className='text-2xl font-bold -mb-1'>Shivam</p>
-                                    <p>2 day ago</p>
-                                </div>
-                            </div>
-                            <div className=''>
-                                <p className='text-xl mx-6'> Can you recommend some devotional practices for building a stronger relationship with Lord Krishna?
-                                    Can you recommend some devotional practices for building a stronger relationship with Lord Krishna?
-                                </p>
-                            </div>
-                        </div>
-                        <div className='w-9/10 bg-[#FFEDCC] mt-5 p-5 rounded-xl'>
-                            <div className='flex m-2'>
-                                <div className='w-[45px] h-[45px] rounded-full bg-green-500 mx-3'>
-                                </div>
-                                <div>
-                                    <p className='text-2xl font-bold -mb-1'>Shivam</p>
-                                    <p>2 day ago</p>
-                                </div>
-                            </div>
-                            <div className=''>
-                                <p className='text-xl mx-6'> Can you recommend some devotional practices for building a stronger relationship with Lord Krishna?
-                                    Can you recommend some devotional practices for building a stronger relationship with Lord Krishna?
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+                    ))}
+
+
+
                     <div className='w-full   border-2 border-[#FFA500] mt-5 p-5 rounded-xl'>
                         <div className='flex m-2'>
                             <div className='w-[45px] h-[45px] rounded-full bg-green-500 mx-3'>
