7839a8c20dc2d370c928c434f5b4d1da620b9762
Ajay-Goswami <goswamiajay2002@gmail.com>
2024-07-07 17:01:40 +0530

    chatroom and profile done

--- client/src/pages/Chatroom.jsx
+++ client/src/pages/Chatroom.jsx
@@ -1,121 +1,169 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 export default function Chatroom() {
-    const { currentUser, loading, error } = useSelector((state) => state.user);
-    const [message, setMessage] = useState('');
-    const [chatData, setChatData] = useState([]);
-    const [activeLink, setActiveLink] = useState('chatroom');
+  const { currentUser, loading, error } = useSelector((state) => state.user);
+  const [message, setMessage] = useState("");
+  const [chatData, setChatData] = useState([]);
+  const [activeLink, setActiveLink] = useState("chatroom");
 
-    const handleLinkClick = (link) => {
-        setActiveLink(link);
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+  };
+
+  const fetchChatData = async () => {
+    try {
+      const res = await fetch(`/api/user/api/chatroom/${currentUser._id}`);
+      const data = await res.json();
+      setChatData(data.data);
+    } catch (error) {
+      console.log("Error occurred while fetching chat data:", error);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // Prevent form from reloading the page
+
+    try {
+      const res = await fetch(`/api/user/api/chatroom/${currentUser._id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ message }), // Send message as an object
+      });
+      const data = await res.json();
+      setChatData(data.data);
+      setMessage(""); // Clear the message input field
+    } catch (error) {
+      console.log("Error occurred while sending the message:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchChatData(); // Initial fetch when the component mounts
+
+    const intervalId = setInterval(() => {
+      fetchChatData();
+    }, 10000); // 10000ms = 10s
+
+    return () => clearInterval(intervalId); // Clean up the interval on component unmount
+  }, []);
+
+  const formatDate = (dateString) => {
+    const options = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
     };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
 
-    const fetchChatData = async () => {
-        try {
-            const res = await fetch(`/api/user/api/chatroom/${currentUser._id}`);
-            const data = await res.json();
-            setChatData(data.data);
-        } catch (error) {
-            console.log("Error occurred while fetching chat data:", error);
-        }
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault(); // Prevent form from reloading the page
-
-        try {
-            const res = await fetch(`/api/user/api/chatroom/${currentUser._id}`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ message }), // Send message as an object
-            });
-            const data = await res.json();
-            setChatData(data.data);
-            setMessage(''); // Clear the message input field
-        } catch (error) {
-            console.log("Error occurred while sending the message:", error);
-        }
-    };
-
-    useEffect(() => {
-        fetchChatData(); // Initial fetch when the component mounts
-
-        const intervalId = setInterval(() => {
-            fetchChatData();
-        }, 10000); // 10000ms = 10s
-
-        return () => clearInterval(intervalId); // Clean up the interval on component unmount
-    }, []);
-
-    const formatDate = (dateString) => {
-        const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
-        return new Date(dateString).toLocaleDateString(undefined, options);
-    };
-
-    return (
-        <div className="max-w-3xl mx-auto px-4">
-            <div className='md:hidden w-[300px] h-[46px] flex justify-between border-2 border-[#008080]'>
-                <div
-                    className={`w-1/2 text-center ${activeLink === 'chatroom' ? 'bg-[#008080]' : ''}`}
-                    onClick={() => handleLinkClick('chatroom')}
-                >
-                    <Link to='/chatroom'>ChatRoom</Link>
-                </div>
-                <div
-                    className={`w-1/2 text-center ${activeLink === 'community' ? 'bg-[#008080]' : ''}`}
-                    onClick={() => handleLinkClick('community')}
-                >
-                    <Link to='/community'>Q & A</Link>
-                </div>
-            </div>
-            <h2 className="text-2xl font-bold mb-4">Chatroom</h2>
-            {loading && <p>Loading...</p>}
-            {error && <p>Error: {error}</p>}
-            {!loading && !error && (
-                <>
-                    <div>
-                        <h3 className="text-xl font-bold mb-2">Chat Messages</h3>
-                        {chatData && chatData.length > 0 ? (
-                            chatData.map((item, index) => (
-                                <div key={index} className={`my-4 flex ${item.userId === currentUser._id ? 'justify-end' : ''}`}>
-                                    <div className={`flex items-start ${item.userId === currentUser._id ? 'flex-row-reverse' : ''}`}>
-                                        <div className="flex-shrink-0">
-                                            {item.photo && <img src={item.photo} alt={`${item.name}'s profile`} className="w-10 h-10 rounded-full" />}
-                                        </div>
-                                        <div className={`ml-2 ${item.userId === currentUser._id ? 'text-right' : 'text-left'}`}>
-                                            <p className="text-lg text-[#FFA500]">{item.name}</p>
-                                            <p className="text-xs text-gray-500">{formatDate(item.createdAt)}</p>
-                                            <div className={`mt-1 p-2 bg-[#FFEDCC] rounded-lg ${item.id === currentUser._id ? 'ml-auto' : ''}`}>
-                                                <p>{item.message}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            ))
-                        ) : (
-                            <p>No messages to display.</p>
-                        )}
-                    </div>
-                </>
-            )}
-            <form onSubmit={handleSubmit} className="mb-4">
-                <div className="flex items-center bg-yellow-400 dark:bg-zinc-800 p-2 rounded-lg">
-                    <input
-                        type="text"
-                        className="border rounded-lg px-4 py-2 w-full mr-2"
-                        placeholder="Type your message..."
-                        value={message}
-                        onChange={(e) => setMessage(e.target.value)}
-                    />
-                    <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-lg">
-                        <img aria-hidden="true" alt="send" src="https://placehold.co/20?text=%E2%86%92" />
-                    </button>
-                </div>
-            </form>
+  return (
+    <div className="max-w-3xl mx-auto px-4 my-10">
+      <div className="md:hidden w-full flex justify-between items-center border-2 border-[#008080] mb-4">
+        <div
+          className={`w-1/2 text-center py-2 ${
+            activeLink === "chatroom"
+              ? "bg-[#008080] text-white"
+              : "text-[#008080]"
+          }`}
+          onClick={() => handleLinkClick("chatroom")}
+        >
+          <Link to="/chatroom">ChatRoom</Link>
         </div>
-    );
+        <div
+          className={`w-1/2 text-center py-2 ${
+            activeLink === "community"
+              ? "bg-[#008080] text-white"
+              : "text-[#008080]"
+          }`}
+          onClick={() => handleLinkClick("community")}
+        >
+          <Link to="/community">Q & A</Link>
+        </div>
+      </div>
+      <h2 className="text-2xl font-bold mb-4">Chatroom</h2>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      {!loading && !error && (
+        <>
+          <div>
+            <h3 className="text-xl font-bold mb-2">Chat Messages</h3>
+            {chatData && chatData.length > 0 ? (
+              chatData.map((item, index) => (
+                <div
+                  key={index}
+                  className={`my-4 flex ${
+                    item.userId === currentUser._id ? "justify-end" : ""
+                  }`}
+                >
+                  <div
+                    className={`flex items-start ${
+                      item.userId === currentUser._id ? "flex-row-reverse" : ""
+                    }`}
+                  >
+                    <div className="flex-shrink-0">
+                      {item.photo && (
+                        <img
+                          src={item.photo}
+                          alt={`${item.name}'s profile`}
+                          className="w-10 h-10 rounded-full"
+                        />
+                      )}
+                    </div>
+                    <div
+                      className={`ml-2 ${
+                        item.userId === currentUser._id
+                          ? "text-right"
+                          : "text-left"
+                      }`}
+                    >
+                      <p className="text-lg text-[#FFA500]">{item.name}</p>
+                      <p className="text-xs text-gray-500">
+                        {formatDate(item.createdAt)}
+                      </p>
+                      <div
+                        className={`mt-1 p-2 bg-[#FFEDCC] rounded-lg ${
+                          item.id === currentUser._id ? "ml-auto" : ""
+                        }`}
+                      >
+                        <p>{item.message}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <p>No messages to display.</p>
+            )}
+          </div>
+        </>
+      )}
+      <form onSubmit={handleSubmit} className="mb-4">
+        <div className="flex items-center bg-yellow-400 dark:bg-zinc-800 p-2 rounded-lg">
+          <input
+            type="text"
+            className="border rounded-lg px-4 py-2 w-full mr-2"
+            placeholder="Type your message..."
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 text-white px-4 py-2 rounded-lg"
+          >
+            <img
+              aria-hidden="true"
+              alt="send"
+              src="https://placehold.co/20?text=%E2%86%92"
+            />
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,7 @@
-import { useSelector } from 'react-redux';
-import { useRef, useState, useEffect } from 'react';
-import bgprofile from '../assets/bgProfile.png'
-import profile from '../assets/profilepic.png'
+import { useSelector } from "react-redux";
+import { useRef, useState, useEffect } from "react";
+import bgprofile from "../assets/bgProfile.png";
+import profile from "../assets/profilepic.png";
 import { FaRegUser } from "react-icons/fa";
 import { MdOutlineMail } from "react-icons/md";
 import { FaMobileAlt } from "react-icons/fa";
@@ -14,9 +14,9 @@ import {
   getStorage,
   ref,
   uploadBytesResumable,
-} from 'firebase/storage';
-import { app } from '../firebase';
-import { useDispatch } from 'react-redux';
+} from "firebase/storage";
+import { app } from "../firebase";
+import { useDispatch } from "react-redux";
 import {
   updateUserStart,
   updateUserSuccess,
@@ -25,7 +25,7 @@ import {
   deleteUserSuccess,
   deleteUserFailure,
   signOut,
-} from '../redux/user/userSlice';
+} from "../redux/user/userSlice";
 
 export default function Profile() {
   const dispatch = useDispatch();
@@ -48,7 +48,7 @@ export default function Profile() {
     const storageRef = ref(storage, fileName);
     const uploadTask = uploadBytesResumable(storageRef, image);
     uploadTask.on(
-      'state_changed',
+      "state_changed",
       (snapshot) => {
         const progress =
           (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
@@ -73,9 +73,9 @@ export default function Profile() {
     try {
       dispatch(updateUserStart());
       const res = await fetch(`/api/user/update/${currentUser._id}`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
       });
@@ -95,7 +95,7 @@ export default function Profile() {
     try {
       dispatch(deleteUserStart());
       const res = await fetch(`/api/user/delete/${currentUser._id}`, {
-        method: 'DELETE',
+        method: "DELETE",
       });
       const data = await res.json();
       if (data.success === false) {
@@ -110,120 +110,162 @@ export default function Profile() {
 
   const handleSignOut = async () => {
     try {
-      await fetch('/api/auth/signout');
-      dispatch(signOut())
+      await fetch("/api/auth/signout");
+      dispatch(signOut());
     } catch (error) {
       console.log(error);
     }
   };
-  return (<>
-    <div className='w-full'>
-      <div className='md:w-4/5 mx-auto'
-        style={{
-          backgroundImage: `url(${bgprofile})`,
-          backgroundSize: 'cover', // or 'contain' depending on your design needs
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat'
-        }}>
-        <div className='h-20'></div>
-
-        <div className='p-3 md:w-2/4 w-4/5 mx-auto border-2 border-[#008080] rounded-3xl bg-white'
-
-
+  return (
+    <>
+      <div className="w-full">
+        <div
+          className="md:w-4/5 mx-auto"
+          style={{
+            backgroundImage: `url(${bgprofile})`,
+            backgroundSize: "cover", // or 'contain' depending on your design needs
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+          }}
         >
+          <div className="h-20"></div>
 
-          <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-            <div className='flex justify-center items-center mx-auto h-[200px] w-[200px]' style={{
-              backgroundImage: `url(${profile})`,
-              backgroundSize: 'cover', // or 'contain' depending on your design needs
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat'
-            }}>
-              <input
-                type='file'
-                ref={fileRef}
-                hidden
-                accept='image/*'
-                onChange={(e) => setImage(e.target.files[0])}
-              />
-              {/* 
+          <div className="p-3 md:w-2/4 w-4/5 mx-auto border-2 border-[#008080] rounded-3xl bg-white">
+            <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+              <div
+                className="flex justify-center items-center mx-auto h-[200px] w-[200px]"
+                style={{
+                  backgroundImage: `url(${profile})`,
+                  backgroundSize: "cover", // or 'contain' depending on your design needs
+                  backgroundPosition: "center",
+                  backgroundRepeat: "no-repeat",
+                }}
+              >
+                <input
+                  type="file"
+                  ref={fileRef}
+                  hidden
+                  accept="image/*"
+                  onChange={(e) => setImage(e.target.files[0])}
+                />
+                {/* 
       firebase storage rules:  
       allow read;
       allow write: if
       request.resource.size < 2 * 1024 * 1024 &&
       request.resource.contentType.matches('image/.*') */}
-              <img
-                src={formData.profilePicture || currentUser.profilePicture}
-                alt='profile'
-                className='h-32 w-32 self-center cursor-pointer rounded-full object-cover mt-2 border-2 border-[#008080]'
-                onClick={() => fileRef.current.click()}
-              />
+                <img
+                  src={formData.profilePicture || currentUser.profilePicture}
+                  alt="profile"
+                  className="h-32 w-32 self-center cursor-pointer rounded-full object-cover mt-2 border-2 border-[#008080]"
+                  onClick={() => fileRef.current.click()}
+                />
+              </div>
+              <p className="text-sm self-center">
+                {imageError ? (
+                  <span className="text-red-700">
+                    Error uploading image (file size must be less than 2 MB)
+                  </span>
+                ) : imagePercent > 0 && imagePercent < 100 ? (
+                  <span className="text-slate-700">{`Uploading: ${imagePercent} %`}</span>
+                ) : imagePercent === 100 ? (
+                  <span className="text-green-700">
+                    Image uploaded successfully
+                  </span>
+                ) : (
+                  ""
+                )}
+              </p>
+
+              <div className="relative mb-2 mt-2 flex items-center">
+                <FaRegUser className=" absolute left-4 mb-0 mt-auto" />
+                <input
+                  type="text"
+                  id="email-address-icon"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 py-2.5"
+                  placeholder="Name of devotee"
+                ></input>
+              </div>
+              <div className="relative mb-2 mt-2 flex items-center">
+                <MdOutlineMail className=" absolute left-4 mb-0 mt-auto" />
+                <input
+                  type="email"
+                  id="email-address-icon"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 py-2.5"
+                  placeholder="Email"
+                ></input>
+              </div>
+              <div className="relative mb-2 mt-2 flex items-center">
+                <FaMobileAlt className=" absolute left-4 mb-0 mt-auto" />
+                <input
+                  type="text"
+                  id="email-address-icon"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 py-2.5"
+                  placeholder="Mobile Number"
+                ></input>
+              </div>
+              <div className="relative mb-2 mt-2 flex items-center">
+                <ImProfile className=" absolute left-4 mb-0 mt-auto" />
+                <input
+                  type="text"
+                  id="email-address-icon"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 py-2.5"
+                  placeholder="Your Country name"
+                ></input>
+              </div>
+              <div className="relative mb-2 mt-2 flex items-center">
+                <MdOutlineRealEstateAgent className=" absolute left-4 mb-0 mt-auto" />
+                <input
+                  type="text"
+                  id="email-address-icon"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 py-2.5"
+                  placeholder="Age"
+                ></input>
+                <div className="relative w-5/6 ml-4">
+                  <GiNurseMale className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
+                  <select
+                    id="gender"
+                    name="gender"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 py-2.5"
+                  >
+                    <option value="">Select Gender</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                  </select>
+                </div>
+              </div>
+
+              <button
+                className="w-full py-2 rounded-md shadow-lg font-medium text-gray-100 block transition duration-300 bg-[#008080] text-xl"
+                type="submit"
+              >
+                Update
+              </button>
+            </form>
+            <div className="flex justify-between mt-5">
+              <span
+                onClick={handleDeleteAccount}
+                className="text-red-700 cursor-pointer"
+              >
+                Delete Account
+              </span>
+              <span
+                onClick={handleSignOut}
+                className="text-red-700 cursor-pointer"
+              >
+                Sign out
+              </span>
             </div>
-            <p className='text-sm self-center'>
-              {imageError ? (
-                <span className='text-red-700'>
-                  Error uploading image (file size must be less than 2 MB)
-                </span>
-              ) : imagePercent > 0 && imagePercent < 100 ? (
-                <span className='text-slate-700'>{`Uploading: ${imagePercent} %`}</span>
-              ) : imagePercent === 100 ? (
-                <span className='text-green-700'>Image uploaded successfully</span>
-              ) : (
-                ''
-              )}
+            <p className="text-red-700 mt-5">
+              {error && "Something went wrong!"}
             </p>
-
-
-            <div className="relative mb-2 mt-2 flex items-center">
-              <FaRegUser className=" absolute left-4 mb-0 mt-auto" />
-              <input type="text" id="email-address-icon" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-3  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Name of devotee"></input>
-            </div>
-            <div className="relative mb-2 mt-2 flex items-center">
-              <MdOutlineMail className=" absolute left-4 mb-0 mt-auto" />
-              <input type="email" id="email-address-icon" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email"></input>
-            </div>
-            <div className="relative mb-2 mt-2 flex items-center">
-              <FaMobileAlt className=" absolute left-4 mb-0 mt-auto" />
-              <input type="text" id="email-address-icon" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Mobile Number"></input>
-            </div>
-            <div className="relative mb-2 mt-2 flex items-center">
-              <ImProfile className=" absolute left-4 mb-0 mt-auto" />
-              <input type="text" id="email-address-icon" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your Country name"></input>
-            </div>
-            <div className="relative mb-2 mt-2 flex items-center">
-              <MdOutlineRealEstateAgent className=" absolute left-4 mb-0 mt-auto" />
-              <input type="text" id="email-address-icon" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mr-3" placeholder="Age"></input>
-              <GiNurseMale className=" absolute right-40 mb-0 mt-auto" />
-              <select id="gender" name="gender" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                <option value="">Select Gender</option>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-              </select>
-            </div>
-
-            <button className="w-full py-2 rounded-md shadow-lg font-medium text-gray-100 block transition duration-300 bg-[#008080] text-xl" type="submit">
-              Update
-            </button>
-          </form>
-          <div className='flex justify-between mt-5'>
-            <span
-              onClick={handleDeleteAccount}
-              className='text-red-700 cursor-pointer'
-            >
-              Delete Account
-            </span>
-            <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>
-              Sign out
-            </span>
+            <p className="text-green-700 mt-5">
+              {updateSuccess && "User is updated successfully!"}
+            </p>
           </div>
-          <p className='text-red-700 mt-5'>{error && 'Something went wrong!'}</p>
-          <p className='text-green-700 mt-5'>
-            {updateSuccess && 'User is updated successfully!'}
-          </p>
         </div>
       </div>
-    </div>
-    <div className='md:hidden h-[150px]'></div>
-  </>
+      <div className="md:hidden h-[150px]"></div>
+    </>
   );
 }
