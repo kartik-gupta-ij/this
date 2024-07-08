aca25f5b346bc01598241250aaeb692f869425ef
Avinash kumar <avinash2y20@gmail.com>
2024-07-09 02:16:10 +0530

    k

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   build: {
-        rollupOptions: {
-            external: ['xlsx'],
-        },
+    rollupOptions: {
+      external: ['xlsx', 'react-chartjs-2'],
     },
+  },
   server: {
     proxy: {
       '/api': {
