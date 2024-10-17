18c3d212825ee7c5920097db323b18b43155476f
Avinash kumar <avinash2y20@gmail.com>
2024-10-17 22:59:57 +0530

    Added front and backend in same port

--- api/index.js
+++ api/index.js
@@ -27,11 +27,11 @@ const __dirname = path.resolve();
 
 const app = express();
 app.use(cors());
-// app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use(express.json());
 
