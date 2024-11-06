c9bbc5385c7d2db4b2daf65122c41a1f00ea4c52
Ananya Jain <78138175+ananyajain10@users.noreply.github.com>
2024-11-06 23:43:02 +0530

    bgf

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
-    port: "https://sadhana-v5rh.onrender.com",
+    port: "5000",
     proxy: {
       '/api': {
         target: "https://sadhnaapi.onrender.com",
