e772942bd2886568dbfc7b0687ff67669e3b0c30
Ananya Jain <78138175+ananyajain10@users.noreply.github.com>
2024-11-07 17:36:08 +0530

    be33fg

--- dist/sw.js
+++ dist/sw.js
@@ -1,2 +1,52 @@
-if(!self.define){let e,s={};const i=(i,n)=>(i=new URL(i+".js",n).href,s[i]||new Promise((s=>{if("document"in self){const e=document.createElement("script");e.src=i,e.onload=s,document.head.appendChild(e)}else e=i,importScripts(i),s()})).then((()=>{let e=s[i];if(!e)throw new Error(`Module ${i} didn’t register its module`);return e})));self.define=(n,r)=>{const t=e||("document"in self?document.currentScript.src:"")||location.href;if(s[t])return;let o={};const l=e=>i(e,t),d={module:{uri:t},exports:o,require:l};s[t]=Promise.all(n.map((e=>d[e]||l(e)))).then((e=>(r(...e),o)))}}define(["./workbox-3e911b1d"],(function(e){"use strict";self.skipWaiting(),e.clientsClaim(),e.precacheAndRoute([{url:"assets/index-BfZ2lkca.js",revision:null},{url:"assets/index-BPXMjdd8.css",revision:null},{url:"index.html",revision:"ec8cc9e40251a325edad342f8f0797cf"},{url:"registerSW.js",revision:"1872c500de691dce40960bb85481de07"},{url:"manifest.webmanifest",revision:"fba0f7e4a496a3ae36032f960b5a2283"}],{}),e.cleanupOutdatedCaches(),e.registerRoute(new e.NavigationRoute(e.createHandlerBoundToURL("index.html")))}));
+if (!self.define) {
+  let e,
+    s = {};
+  const i = (i, n) => (
+    (i = new URL(i + ".js", n).href),
+    s[i] ||
+      new Promise((s) => {
+        if ("document" in self) {
+          const e = document.createElement("script");
+          (e.src = i), (e.onload = s), document.head.appendChild(e);
+        } else (e = i), importScripts(i), s();
+      }).then(() => {
+        let e = s[i];
+        if (!e) throw new Error(`Module ${i} didn’t register its module`);
+        return e;
+      })
+  );
+  self.define = (n, r) => {
+    const t =
+      e ||
+      ("document" in self ? document.currentScript.src : "") ||
+      location.href;
+    if (s[t]) return;
+    let o = {};
+    const l = (e) => i(e, t),
+      d = { module: { uri: t }, exports: o, require: l };
+    s[t] = Promise.all(n.map((e) => d[e] || l(e))).then((e) => (r(...e), o));
+  };
+}
+define(["./workbox-3e911b1d"], function (e) {
+  "use strict";
+  self.skipWaiting(),
+    e.clientsClaim(),
+    e.precacheAndRoute(
+      [
+        { url: "assets/index-BfZ2lkca.js", revision: null },
+        { url: "assets/index-BPXMjdd8.css", revision: null },
+        { url: "index.html", revision: "ec8cc9e40251a325edad342f8f0797cf" },
+        { url: "registerSW.js", revision: "1872c500de691dce40960bb85481de07" },
+        {
+          url: "manifest.webmanifest",
+          revision: "fba0f7e4a496a3ae36032f960b5a2283",
+        },
+      ],
+      {}
+    ),
+    e.cleanupOutdatedCaches(),
+    e.registerRoute(
+      new e.NavigationRoute(e.createHandlerBoundToURL("index.html"))
+    );
+});
 //# sourceMappingURL=sw.js.map
